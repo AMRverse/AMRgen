b40531981347e7738d98be3361bf5e540b4d19e5
silargi <silargi@users.noreply.github.com>
2026-03-04 16:59:47 +0000

    Adding data files for the Concordance Vignette

---
+++ data-raw/prepare_data_concordance_vignette.R
@@ -0,0 +1,11 @@
+# E. coli data for the Concordance vignette from Mills et al, Genome Medicine (2022) 14:147
+
+library(readr)
+# Phenotypic data obtained from the EBI AMR portal with download_ebi function, reformatted but NOT reinterpreted
+pheno_eco_2075 <- read_tsv("/Users/silargi/Documents/ESGEM-AMR/AMRgen/data-raw/pheno_eco_2075.tsv.gz") 
+
+# Phenotypic data obtained from All The Bacteria
+geno_eco_2075 <- read_tsv("/Users/silargi/Documents/ESGEM-AMR/AMRgen/data-raw/geno_eco_2075.tsv.gz")
+
+usethis::use_data(pheno_eco_2075, internal = FALSE, overwrite = TRUE)
+usethis::use_data(geno_eco_2075, internal = FALSE, overwrite = TRUE)

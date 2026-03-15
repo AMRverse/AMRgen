04131470e649541412291943ac21ae41f174a548
karatsang <karaktsang@gmail.com>
2026-03-15 13:23:12 -0400

    reducing rgi_v606_EuSCAPE.txt.gz file size

--- R/data.R
+++ R/data.R
@@ -587,8 +587,9 @@
 #'
 #' Raw RGI v6.0.6 results file for Klebsiella pneumoniae from EuSCAPE project, one AMR determinant per row. 
 #' Includes only Perfect and Strict hits
+#' Columns `Predicted_DNA`, `Predicted_Protein`, and `CARD_Protein_Sequence` were removed to reduce file size.
 #' 
-#' @format `rgi_EuSCAPE_raw` A data frame with 59,403 rows and 28 columns:
+#' @format `rgi_EuSCAPE_raw` A data frame with 59,403 rows and 25 columns:
 #' - `ORF_ID`: Sample identifier
 #' - ...: RGI results columns
 #' @source ENA BioProject [PRJEB10018](https://www.ebi.ac.uk/ena/browser/view/PRJEB10018).

31876bfc9e91c8e0a6988e98d99d8a5f6b231a5f
karatsang <karaktsang@gmail.com>
2026-03-17 12:32:02 -0400

    editing Kleborate raw data input files

--- data-raw/prep_kleborate_data.R
+++ data-raw/prep_kleborate_data.R
@@ -1,9 +1,9 @@
-# Load example kleborate results file
-kleborate_raw <- read_tsv("data-raw/EuSCAPE_kleborate.tsv.gz")
+# Load example kleborate results file - Development Branch March 17, 2026
+kleborate_raw <- read_tsv("data-raw/EuSCAPE_kleborate_vDevBranch03172026.txt.gz")
 usethis::use_data(kleborate_raw, internal = FALSE, overwrite = TRUE)
 
 # Load example kleborate results file - earlier version prior to switching to HGVS nomenclature
-kleborate_raw_v313 <- read_tsv("data-raw/EuSCAPE_Kleborate_v3.1.3.tsv.gz")
+kleborate_raw_v313 <- read_tsv("data-raw/EuSCAPE_Kleborate_v3.1.3.txt.gz")
 usethis::use_data(kleborate_raw_v313, internal = FALSE, overwrite = TRUE)
 
 # load file mapping kleborate AMR columns to AMR pkg drug classes

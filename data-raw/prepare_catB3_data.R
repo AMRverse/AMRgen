014972309b67ed60f881a5002cdc535e1e3bf9df
Richard Goodman <richard.goodman@MP4414s-MacBook-Pro.local>
2026-03-17 16:19:25 +0000

    Add vignette for E. coli catB3 and document internal datasets

---
+++ data-raw/prepare_catB3_data.R
@@ -0,0 +1,41 @@
+## General information
+# Genotype files have been obtained using AMRfinderplus 4.2.7 
+
+####
+# 2. DASSIM
+####
+
+## 2.5 Importing phenotype data
+
+# Read in DASSIM metdata 
+DASSIM_pheno = read_tsv("DASSIM_BSI_metadata.tsv")
+usethis::use_data(DASSIM_pheno, internal = FALSE, overwrite = TRUE)
+
+# Read in the AST data from blantyre ESBL
+btESBL_AST = read_csv("DASSIM_btESBL_AST.csv")
+usethis::use_data(btESBL_AST, internal = FALSE, overwrite = TRUE)
+
+
+## 2.6 Importing genotype data
+
+# Use import_amrfp() for AMRfinderplus data
+DASSIM_geno = import_amrfp("DASSIM_BSI_Malawi_amrfinder_v4.0.23_geno.tsv")
+usethis::use_data(DASSIM_geno, internal = FALSE, overwrite = TRUE)
+
+####
+# 3. NCBI
+####
+
+# 3.2  Reading in genotype and phenotype data
+
+# import phenotype data
+NCBI_AST_CHL = read_tsv("CHL_Ecoli_asts.tsv")
+NCBI_AST_CHL = NCBI_AST_CHL %>% rename("BioSample" = "#BioSample")
+usethis::use_data(NCBI_AST_CHL, internal = FALSE, overwrite = TRUE)
+
+
+# import genotype
+MICROBIGGE_CATB3 = read_tsv("catB3_Ecoli_microbigge.tsv")
+usethis::use_data(MICROBIGGE_CATB3, internal = FALSE, overwrite = TRUE)
+MICROBIGGE_CHLR = read_tsv("CHL-R_Ecoli_microbigge.tsv")
+usethis::use_data(MICROBIGGE_CHLR, internal = FALSE, overwrite = TRUE)
